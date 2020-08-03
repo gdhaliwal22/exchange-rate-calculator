1a95abe45a347b0fb35c417d696be8e057d6dd74
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-03 11:15:37 -0700

    calculate function updating inputs with currency from API and converting

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ function calculate() {
     .then((data) => {
       //   console.log(data);
       const rate = data.rates[currency_two];
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
 }
 
