0ad259c06d0dfe449eac377cd82842e2d6572020
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-03 11:31:10 -0700

    swap button functional

--- script.js
+++ script.js
@@ -14,18 +14,26 @@ function calculate() {
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
     .then((res) => res.json())
     .then((data) => {
-      //   console.log(data);
+      // console.log(data);
       const rate = data.rates[currency_two];
 
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
 }
-
 // Event listeners
 currencyEl_one.addEventListener("change", calculate);
 amountEl_one.addEventListener("input", calculate);
 currencyEl_two.addEventListener("change", calculate);
 amountEl_two.addEventListener("input", calculate);
+
+// Swap currency conversion
+swap.addEventListener("click", () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
 
 calculate();
