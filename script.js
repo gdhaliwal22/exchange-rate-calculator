a283174954a27ebe7126d2a99ed4ce7934b3c6d0
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-08-03 11:14:56 -0700

    fetching currency from API

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const currencyEl_one = document.getElementById("currency-one");
-const currencyEl_two = document.getElementById("currency-two");
 const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
 const amountEl_two = document.getElementById("amount-two");
 
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
-function calculate() {}
+// Fetch exchange rates and update the DOM
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data);
+      const rate = data.rates[currency_two];
+    });
+}
+
+// Event listeners
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
 
 calculate();
